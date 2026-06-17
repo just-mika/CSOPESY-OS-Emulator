306a82f886bc1340bc9ddd3d88703ada24778b05
justine-wee <justine_wee@dlsu.edu.ph>
2026-06-17 14:44:07 +0800

    Update Process.cpp

--- src/Process.cpp
+++ src/Process.cpp
@@ -1,10 +1,32 @@
 #include "Process.h"
+#include <iomanip>
+#include <sstream>
 
 Process::Process(int pid, std::string name)
 {
-	this->pID = pid;
-	this->name = name;
+    this->pID = pid;
+    this->name = name;
+    
+    this->creationTime = std::time(nullptr);
 }
+
+std::string Process::getFormattedCreationTime() const 
+{
+    std::tm tm_struct;
+    
+    // Thread-safe local time extraction (for both Windows and Mac)
+	// Reference: https://cplusplus.com/forum/general/189594/
+#ifdef _WIN32
+    localtime_s(&tm_struct, &this->creationTime);
+#else
+    localtime_r(&this->creationTime, &tm_struct);
+#endif
+
+    std::stringstream ss;
+    ss << std::put_time(&tm_struct, "%m/%d/%Y %I:%M:%S%p");
+    return ss.str();
+}
+
 
 void Process::addCommand(std::shared_ptr<ICommand> command)
 {
